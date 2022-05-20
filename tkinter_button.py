928a5e6478947049db645615a42ac40cafb51e6b
HarikaHarix <harikabatchala@gmail.com>
2022-05-20 22:54:47 +0530

    tkinter learning

---
+++ tkinter_button.py
@@ -0,0 +1,5 @@
+from tkinter import *
+window=Tk()
+b1=Button(window,text="enter",bg="red",fg="white")
+b1.grid(row=0,column=1)
+window.mainloop()
---
+++ tkinter_entry.py
@@ -0,0 +1,5 @@
+from tkinter import *
+window=Tk()
+e1= Entry(window,width=20,bd=5,font=("Calibri",20))  #bd represents border
+e1.pack()
+window.mainloop()
